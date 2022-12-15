9b2ee0a61b3533d4a091408859e987d728dc7343
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-12-15 05:51:18 +0000

    16章End

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,21 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = PostImage.find(params[:id]) 
+    @user = User.find(params[:id]) 
     
   end
+  
+  def update
+    @user = User.find(params[:id]) 
+    @user.update(user_params)
+    redirect_to user_path(@user.id) 
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit] 
+  resources :users, only: [:show, :edit, :update] 
   
 end
